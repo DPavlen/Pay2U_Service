351d31d0324e4cac2806a934298945cde86a4aef
Герман Леонтьев <gerleonsto@gmail.com>
2024-03-22 00:00:17 +0300

    добавление модели пользователя ver.4.0

--- Pay2U_Service/user/models.py
+++ Pay2U_Service/user/models.py
@@ -9,7 +9,9 @@ class User(AbstractUser):
     email = models.EmailField(null=True, blank=True, unique=True)
     login = models.CharField(max_length=25, blank=True, verbose_name="Логин")
     full_name = models.CharField(max_length=255, blank=True, verbose_name="Полное имя")
-    phone = PhoneNumberField(max_length=20, null=True, blank=True, verbose_name="Телефон")
+    phone = PhoneNumberField(
+        max_length=20, null=True, blank=True, verbose_name="Телефон"
+    )
     first_enter = models.BooleanField(default=True, tverbose_name="Флаг первого фхода")
 
     USERNAME_FIELD = "email"
@@ -27,8 +29,12 @@ class User(AbstractUser):
 
 
 class UserIcon(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
-    icon = models.ImageField(verbose_name='Фото профиля', upload_to='users/profile_photo')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    icon = models.ImageField(
+        verbose_name="Фото профиля", upload_to="users/profile_photo"
+    )
 
     def __str__(self):
         return self.user
